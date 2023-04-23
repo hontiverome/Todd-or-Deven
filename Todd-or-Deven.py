5c349058684b3c88afa7071c1f7eeaf5e9f8c974
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-04-23 15:12:35 +0800

    added method, working code.

--- Todd-or-Deven.py
+++ Todd-or-Deven.py
@@ -4,19 +4,23 @@
 # the second file will be named triple.txt containing the cube of all odd numbers found in the integers.txt
 
 # define the method/function
-# read and write the input and output files
-with open("integers.txt", "r") as integer_file, open ("squared_even.txt", "w") as even_file, open ("cubed_odd.txt", "w") as odd_file:
-    # put the numbers from the read file into a list and convert it into integers
-    integers=[line.strip()for line in integer_file]
-    integer_list=[int(j) for j in integers]
-    # iterate the list
-    for num in integer_list:
-        # identify whether integer is even or odd using mod
-        # if even, square it, and write it to "squared_even.txt" file
-        if num%2==0:
-            squared=num**2
-            even_file.write(str(squared)+'\n')
-        # if odd, cube it, and write it to "cubed_odd.txt" file
-        else:
-            cubed=num**3
-            odd_file.write(str(cubed)+'\n')
+# read and write the input and output filesd
+def sEven_cOdd_itegers():
+    with open("integers.txt", "r") as integer_file, open ("squared_even.txt", "w") as even_file, open ("cubed_odd.txt", "w") as odd_file:
+        # put the numbers from the read file into a list and convert it into integers
+        integers=[line.strip()for line in integer_file]
+        integer_list=[int(j) for j in integers]
+        # iterate the list
+        for num in integer_list:
+            # identify whether integer is even or odd using mod
+            # if even, square it, and write it to "squared_even.txt" file
+            if num%2==0:
+                squared=num**2
+                even_file.write(str(squared)+'\n')
+            # if odd, cube it, and write it to "cubed_odd.txt" file
+            else:
+                cubed=num**3
+                odd_file.write(str(cubed)+'\n')
+
+# Execute the code
+sEven_cOdd_itegers()
