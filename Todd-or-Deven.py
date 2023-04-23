df46ce5946ac5a7f72e938a62570981cab26ef57
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-04-23 15:00:26 +0800

    added pseudocodes

--- Todd-or-Deven.py
+++ Todd-or-Deven.py
@@ -2,3 +2,11 @@
 # after reading the source text file named integers.txt that cotains 20 integers. 
 # The first output will be named double.txt containing the square of all even integers found in integers.txt  
 # the second file will be named triple.txt containing the cube of all odd numbers found in the integers.txt
+
+# define the method/function
+# read and write the input and output files
+# put the numbers from the read file into a list and convert it into integers
+# iterate the list
+# identify whether integer is even or odd using mod
+# if even, square it, and write it to "squared_even.txt" file
+# if odd, cube it, and write it to "cubed_odd.txt" file
