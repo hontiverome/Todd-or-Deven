e4db4b0e0160eae021ced725910977c621f8896b
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-04-23 15:07:39 +0800

    added iterations, list, identifier, and squared

--- Todd-or-Deven.py
+++ Todd-or-Deven.py
@@ -8,7 +8,12 @@
 with open("integers.txt", "r") as integer_file, open ("squared_even.txt", "w") as even_file, open ("cubed_odd.txt", "w") as odd_file:
     # put the numbers from the read file into a list and convert it into integers
     integers=[line.strip()for line in integer_file]
-# iterate the list
-# identify whether integer is even or odd using mod
-# if even, square it, and write it to "squared_even.txt" file
+    integer_list=[int(j) for j in integers]
+    # iterate the list
+    for num in integer_list:
+        # identify whether integer is even or odd using mod
+        if num%2==0:
+            squared=num**2
+            even_file.write(str(squared)+'\n')
+        # if even, square it, and write it to "squared_even.txt" file
 # if odd, cube it, and write it to "cubed_odd.txt" file
