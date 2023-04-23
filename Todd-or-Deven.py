543a9c4d4af5cfaa571062da89a8fc623020ef46
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-04-23 15:03:11 +0800

    open read write files

--- Todd-or-Deven.py
+++ Todd-or-Deven.py
@@ -5,6 +5,8 @@
 
 # define the method/function
 # read and write the input and output files
+with open("integers.txt", "r") as integer_file, open ("squared_even.txt", "w") as even_file, open ("cubed_odd.txt", "w") as odd_file:
+    
 # put the numbers from the read file into a list and convert it into integers
 # iterate the list
 # identify whether integer is even or odd using mod
