f2caa6e20533ca24fe4faf84a809ab9336027e0b
Jerome Andrei O. Hontiveros <jrmhntvrs@gmail.com>
2023-04-23 14:57:30 +0800

    adjusted directions

--- Todd-or-Deven.py
+++ Todd-or-Deven.py
@@ -1,3 +1,4 @@
-# Write a method in python that will create two seperate text files after reading the source text file named integers.txt
-# that cotains 20 integers. The first output will be named double.txt containing the square of all even integers found in
-# integers.txt and the second file will be named triple.txt containing the cube of all odd numbers found in the integers.txtx
+# Write a method in python that will create two seperate text files 
+# after reading the source text file named integers.txt that cotains 20 integers. 
+# The first output will be named double.txt containing the square of all even integers found in integers.txt  
+# the second file will be named triple.txt containing the cube of all odd numbers found in the integers.txt
